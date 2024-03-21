bd7fd58532459197c8bd824e52c9ffe64a87ebdc
Vitali Klaskou <73692256+kolosokvit@users.noreply.github.com>
2024-03-21 11:41:53 +0300

    Update SOLUTION.sql lesson8

--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-SELECT MIN (birthday) FROM student;
+SELECT MAX (birthday) FROM student;
 SELECT MIN (payment_date) FROM payment;
 SELECT AVG (mark) FROM mark INNER JOIN subject ON mark.subject_id = subject.id WHERE subject.name = 'Math';
 SELECT MIN (amount) FROM payment INNER JOIN paymenttype ON payment.type_id = paymenttype.id WHERE paymenttype.name = 'WEEKLY';

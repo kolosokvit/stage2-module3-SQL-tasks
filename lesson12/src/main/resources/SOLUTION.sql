7a6145b92ef6b05f510da512a4ece7cd200a473e
Vitali Klaskou <73692256+kolosokvit@users.noreply.github.com>
2024-03-22 11:05:49 +0300

    Update SOLUTION.sql lesson12

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,11 +1,3 @@
-ALTER TABLE mark DROP CONSTRAINT mark_student_id_fkey;
-ALTER TABLE mark ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY (student_id) REFERENCES student(id) ON DELETE CASCADE;
-ALTER TABLE payment DROP CONSTRAINT payment_student_id_fkey;
-ALTER TABLE payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY (student_id) REFERENCES student(id) ON DELETE CASCADE;
 DELETE FROM student WHERE grade > 4;
-
-ALTER TABLE payment DROP CONSTRAINT payment_type_id_fkey;
-ALTER TABLE payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY (type_id) REFERENCES paymenttype(id) ON DELETE CASCADE;
 DELETE FROM paymenttype WHERE name = 'DAILY';
-
 DELETE FROM mark WHERE mark < 7;

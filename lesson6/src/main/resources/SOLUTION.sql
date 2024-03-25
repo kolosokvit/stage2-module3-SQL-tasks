a156fd85e24092047d0b6643b6fc24736ab3d892
Vitali Klaskou <73692256+kolosokvit@users.noreply.github.com>
2024-03-25 14:12:29 +0300

    Update SOLUTION.sql lesson6

--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 SELECT * FROM payment INNER JOIN paymenttype ON payment.type_id = paymenttype.id WHERE paymenttype.name = 'MONTHLY';
 SELECT * FROM mark INNER JOIN subject ON mark.subject_id = subject.id WHERE subject.name = 'Art';
-SELECT * FROM student INNER JOIN payment ON student.id = payment.student_id INNER JOIN paymenttype ON payment.type_id = paymenttype.id WHERE paymenttype.name = 'WEEKLY';
+SELECT FROM student WHERE id IN (SELECT DISTINCT student_id FROM payment WHERE type_id IN (SELECT id FROM paymenttype WHERE name = 'WEEKLY'));
 SELECT * FROM student INNER JOIN mark ON student.id = mark.student_id INNER JOIN subject ON mark.subject_id = subject.id WHERE subject.name = 'Math';

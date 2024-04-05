8480b65eb057aa167207c959759b1fe2cd17696b
Vitali Klaskou <73692256+kolosokvit@users.noreply.github.com>
2024-04-05 11:43:57 +0300

    Update SOLUTION.sql lesson5

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -1,7 +1,7 @@
 SELECT * FROM payment  WHERE amount >= 500;
 SELECT * FROM student WHERE DATE_PART('YEAR', AGE(CURRENT_DATE, birthday)) > 20;
 SELECT * FROM student WHERE groupnumber = 10 AND DATE_PART('YEAR', AGE(CURRENT_DATE, birthday)) < 20;
-SELECT * FROM student WHERE name = 'Mike' OR groupnumber = 4 OR groupnumber = 5 OR groupnumber = 6;
+SELECT * FROM student WHERE name LIKE 'Mike%' OR groupnumber = 4 OR groupnumber = 5 OR groupnumber = 6;
 SELECT * FROM payment WHERE payment_date > (CURRENT_DATE - interval '8 month');
 SELECT * FROM student WHERE name LIKE 'A%';
-SELECT * FROM student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
+SELECT * FROM student WHERE (name LIKE 'Roxi%' AND groupnumber = 4) OR (name LIKE 'Tallie%' AND groupnumber = 9);

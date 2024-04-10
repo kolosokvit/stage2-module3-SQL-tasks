6843ac98019664d0a157c03c00fc326564dafe8d
Vitali Klaskou <73692256+kolosokvit@users.noreply.github.com>
2024-04-10 11:03:56 +0300

    Update SOLUTION.sql lesson 9

--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,6 +1,3 @@
 SELECT * FROM student WHERE id IN (SELECT student_id FROM mark GROUP BY student_id HAVING AVG(mark) > 8);
 SELECT id, name FROM student WHERE id IN (SELECT student_id FROM mark GROUP BY student_id HAVING MIN(mark) > 7);
-SELECT DATE_PART ('year', payment_date::TIMESTAMP) FROM payment;
-CREATE VIEW payment_year AS SELECT student_id, DATE_PART ('year', payment_date::TIMESTAMP) AS payment_year FROM payment;
-CREATE VIEW number_of_payments AS SELECT student_id, COUNT(*) FROM payment_year WHERE payment_year = 2019 GROUP BY student_id;
-SELECT id, name FROM student WHERE id IN (SELECT student_id FROM number_of_payments WHERE count > 2);
+SELECT * FROM student WHERE id IN (SELECT student_id FROM payment WHERE TO_CHAR(payment_date, 'YYYY/MM/DD HH12:MM:SS') LIKE '2019%' GROUP BY student_id HAVING COUNT(*) > 2);

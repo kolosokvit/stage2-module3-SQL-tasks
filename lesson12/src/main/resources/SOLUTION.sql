3dd3491bb6d61a54b181d0057439daec2648d93b
Vitali Klaskou <73692256+kolosokvit@users.noreply.github.com>
2024-04-15 14:25:24 +0300

    Update SOLUTION.sql lesson12

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,3 +1,3 @@
-DELETE FROM student WHERE grade > 4;
+DELETE FROM student WHERE id IN (SELECT student_id FROM mark GROUP BY student_id HAVING MIN(mark) < 4);
 DELETE FROM paymenttype WHERE name = 'DAILY';
 DELETE FROM mark WHERE mark < 7;

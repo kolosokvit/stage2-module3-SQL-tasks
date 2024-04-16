25a53f980bbe4a70ca388bbf98822f512783ea0e
Vitali Klaskou <73692256+kolosokvit@users.noreply.github.com>
2024-04-16 13:25:43 +0300

    Update SOLUTION.sql lesson10

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,2 +1,2 @@
 SELECT * FROM subject WHERE id IN (SELECT subject_id FROM mark GROUP BY subject_id HAVING AVG(mark) > (SELECT AVG(mark) FROM mark));
-SELECT * FROM student WHERE id IN (SELECT student_id FROM payment GROUP BY student_id HAVING SUM(amount) < (SELECT AVG(amount) FROM payment));
+SELECT * FROM student WHERE id IN (SELECT student_id FROM payment GROUP BY student_id HAVING AVG(amount) < (SELECT AVG(amount) FROM payment));
